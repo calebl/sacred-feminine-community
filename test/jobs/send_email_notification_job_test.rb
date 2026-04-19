16c7a82d122f7d780f7a5cc20b8cb7e68263cd99
Caleb LeNoir <calebl@users.noreply.github.com>
2026-04-18 20:47:02 -0400

    add missing test coverage

--- test/jobs/send_email_notification_job_test.rb
+++ test/jobs/send_email_notification_job_test.rb
@@ -109,6 +109,56 @@ class SendEmailNotificationJobTest < ActiveJob::TestCase
     end
   end
 
+  test "sends email for direct_message when the preference is enabled" do
+    dm_notification = Notification.create!(
+      user: @user, actor: @actor,
+      event_type: "direct_message", title: @actor.name,
+      body: "Sent you a private message", path: "/conversations/1"
+    )
+
+    assert_emails 1 do
+      SendEmailNotificationJob.perform_now(dm_notification.id)
+    end
+  end
+
+  test "does not send direct_message email when the preference is disabled" do
+    @user.update!(email_on_direct_message: false)
+    dm_notification = Notification.create!(
+      user: @user, actor: @actor,
+      event_type: "direct_message", title: @actor.name,
+      body: "Sent you a private message", path: "/conversations/1"
+    )
+
+    assert_no_emails do
+      SendEmailNotificationJob.perform_now(dm_notification.id)
+    end
+  end
+
+  test "sends email for new_comment when the preference is enabled" do
+    comment_notification = Notification.create!(
+      user: @user, actor: @actor,
+      event_type: "new_comment", title: @actor.name,
+      body: "Commented on your post", path: "/cohorts/1/posts/1"
+    )
+
+    assert_emails 1 do
+      SendEmailNotificationJob.perform_now(comment_notification.id)
+    end
+  end
+
+  test "does not send new_comment email when the preference is disabled" do
+    @user.update!(email_on_new_comment: false)
+    comment_notification = Notification.create!(
+      user: @user, actor: @actor,
+      event_type: "new_comment", title: @actor.name,
+      body: "Commented on your post", path: "/cohorts/1/posts/1"
+    )
+
+    assert_no_emails do
+      SendEmailNotificationJob.perform_now(comment_notification.id)
+    end
+  end
+
   test "help_request_reply email is suppressed by master toggle" do
     @user.update!(email_notifications_enabled: false)
     reply_notification = Notification.create!(
