8944ce4de3a2f5429fddc3667570b096cc534dcb
Caleb LeNoir <calebl@users.noreply.github.com>
2026-04-27 12:44:45 -0400

    enable traces

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,6 +7,14 @@ Sentry.init do |config|
 
   # Add data like request headers and IP for users
   config.send_default_pii = true
+  config.traces_sample_rate = 1.0
+
+  config.traces_sampler = lambda do |sampling_context|
+    # sampling_context[:transaction_context] contains the information about the transaction
+    # sampling_context[:parent_sampled] contains the transaction's parent's sample decision
+    true # return value can be a boolean or a float between 0.0 and 1.0
+  end
+
 
   # Enable sending logs to Sentry
   config.enable_logs = true
