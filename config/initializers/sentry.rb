7f19508b026d90b3e3a3c5aeb202c3b610aaa0e9
Caleb LeNoir <calebl@users.noreply.github.com>
2026-04-27 12:47:30 -0400

    remove duplicate settings

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -9,13 +9,6 @@ Sentry.init do |config|
   config.send_default_pii = true
   config.traces_sample_rate = 1.0
 
-  config.traces_sampler = lambda do |sampling_context|
-    # sampling_context[:transaction_context] contains the information about the transaction
-    # sampling_context[:parent_sampled] contains the transaction's parent's sample decision
-    true # return value can be a boolean or a float between 0.0 and 1.0
-  end
-
-
   # Enable sending logs to Sentry
   config.enable_logs = true
   # Patch Ruby logger to forward logs
